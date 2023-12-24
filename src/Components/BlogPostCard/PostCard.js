7caf6bd87a5f7bce3b7dee1ec6359771a26d399d
taylanacikgoz <acikgozta@gmail.com>
2023-12-25 00:11:21 +0300

    continue on singlePost

--- src/Components/BlogPostCard/PostCard.js
+++ src/Components/BlogPostCard/PostCard.js
@@ -6,37 +6,40 @@ import Rocket from "../../../public/Images/Rocket.png";
 export const PostCard = ({ title, body, id }) => {
   return (
     <Link
+      key={id}
       href={`/blog/${id}`}
-      className="w-full lg:w-1/2 border border-solid border-yellow-500"
+      className="w-full overflow-hidden rounded-lg"
     >
-      <div className="flex flex-col lg:flex lg:flex-row gap-5 border border-solid ">
-        <div className=" w-full lg:w-1/2 animate-pulse">
+      <div className="flex flex-1 h-full flex-col md:flex-row items-start gap-4 bg-[#010b28] p-5 rounded-lg">
+        <div className="flex-shrink-0 w-36 h-36 relative rounded-full">
           <Image
             src={`https://picsum.photos/200?random=${id}`}
-            width={400}
-            height={400}
+            fill
             alt="Post Figure"
-            className="rounded-full object-cover"
+            className="object-cover rounded-full"
           />
         </div>
 
-        <div className="w-full lg:w-1/2 flex flex-col gap-3 justify-between">
-          <h2 className="font-bold sm:text-base md:text-xl lg:text-2xl first-letter:text-4xl">
+        <div className="flex flex-1 flex-col gap-2 justify-between">
+          <h2 className="font-bold text-xl first-letter:text-4xl antialiased ...">
             {title}
           </h2>
-          <span className="text-sm">June 15, 2023</span>
-          <br />
-          <div className="flex flex-row gap-3 items-center">
-            <span className="underline decoration-2 decoration-wavy hover:underline-offset-2 hover:underline hover:decoration-red-500">
-              Read More
-            </span>
-            <Image
-              src={Rocket}
-              alt=""
-              width={56}
-              height={56}
-              className="object-cover transition ease-in-out delay-3000 hover:-translate-y-20"
-            />
+          <div>
+            <p className="text-[8px] italic ...">Posted on June 15, 2023</p>
+            <div className="flex flex-row gap-3 items-center pt-2">
+              <span className="underline decoration-2 decoration-wavy hover:underline-offset-2 hover:decoration-red-500">
+                Read More
+              </span>
+
+              <div className="w-14 h-14 relative">
+                <Image
+                  src={Rocket}
+                  alt="Read more rocket icon"
+                  fill
+                  className="object-cover transition ease-in-out delay-3000 hover:-translate-y-20"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,16 +5,18 @@ import Rocket from "../../../public/Images/Rocket.png";
 import { PostCard } from "@/Components/BlogPostCard/PostCard";
 
 async function getPosts() {
-  const res = await fetch("https://dummyjson.com/posts");
+  const res = await fetch("https://dummyjson.com/posts", { cache: "no-store" });
   return res.json();
 }
 export default async function Blog() {
   const { posts } = await getPosts();
 
   return (
-    <div className="bg-black text-white flex flex-wrap p-10 border-2 border-solid">
+    <div className="w-full bg-black text-white flex flex-wrap p-10">
       {posts.map((post) => (
-        <PostCard key={post.id} {...post} />
+        <div className="p-10 w-1/2">
+          <PostCard {...post} />
+        </div>
       ))}
     </div>
   );
